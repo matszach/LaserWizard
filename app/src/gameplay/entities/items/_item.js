aa16898925cd531263689a8b717ba1909569ce96
matszach <lkaszubowski@gmail.com>
2020-01-04 17:12:43 +0100

    Item class tileX and tileY fields

--- app/src/gameplay/entities/items/_item.js
+++ app/src/gameplay/entities/items/_item.js
@@ -1,9 +1,17 @@
 class _Item extends _Entity{
 
+    // ==================== fields ====================
+    tileX;              // x position in tileset
+    tileY;              // y position in tileset
+
+    // ==================== constructor ====================
     constructor(){
         super();
+        this.tileX = 0;
+        this.tileY = 0;
     }
 
+    // ==================== methods ====================
     _onCollisionWithPlayer(p){
        if(this.shouldPickUp(p)){
            this.onPickUp(p);
