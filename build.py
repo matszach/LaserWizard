a6cb3b59b5ecd1e59855edcdb1295927f26e01a6
matszach <lkaszubowski@gmail.com>
2020-01-14 22:15:18 +0100

    better build scripts

---
+++ build.py
@@ -0,0 +1,31 @@
+import os
+
+
+version = 0
+subversion = 3
+package_name = f'laser-wizard-{version}-{subversion}'
+
+commands = [
+	f'rmdir /q /s {package_name}',
+	f'electron-packager app laser-wizard --platform=win32 --arch=x64 --overwrite',
+	f'rename laser-wizard-win32-x64 {package_name}',
+	f'cd {package_name}\\resources',
+	f'asar pack app {package_name}\\resources\\app.asar',
+	f'rmdir /q /s {package_name}\\resources\\app',
+	f'mkdir {package_name}\\resources\\app\\assets\\images\\icons',
+	f'xcopy app\\assets\\images\\icons {package_name}\\resources\\app\\assets\\images\\icons',
+	f'rmdir /q /s {package_name}\\swiftshader',
+	f'rmdir /q /s {package_name}\\locales',
+	f'del /f {package_name}\\chrome_100_percent.pak',
+	f'del /f {package_name}\\chrome_200_percent.pak',
+	f'del /f {package_name}\\LICENSES.chromium.html',
+	f'del /f {package_name}\\LICENSE',
+	f'del /f {package_name}\\version',
+	f'del /f {package_name}\\snapshot_blob.bin',
+	f'del /f {package_name}\\d3dcompiler_47.dll'
+]
+
+for c in commands:
+	print(f'>> {c}')
+	os.system(c)
+	
--- build2.cmd
+++
@@ -1,4 +0,0 @@
-rename laser-wizard-win32-x64 laser-wizard
-cd laser-wizard\resources
-asar pack app app.asar
-cmd /k
