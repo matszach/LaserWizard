ecde7cb2c0ca1f0075f7d603544d3fc064bac42e
matszach <lkaszubowski@gmail.com>
2020-01-20 00:01:36 +0100

    door opening projectile animations

--- app/src/gameplay/stage/beacons/doorBeacon.js
+++ app/src/gameplay/stage/beacons/doorBeacon.js
@@ -18,7 +18,12 @@ class DoorBeacon extends _Beacon {
         this.doorTileIds.forEach(t => {
             StageManager.currentStage.wallIds[t[0]][t[1]] = [0, 0]; // empty tile 
             StageManager.currentStage.collisionMap[t[0]][t[1]] = 0; // pass-through tile
+            this._onTileRemoved(t[0], t[1]);
         });
+    }
+
+    _onTileRemoved(x, y){
+        // abstract
     }
 
 }
@@ -27,10 +32,18 @@ class MagentaDoorBeacon extends DoorBeacon {
     _shoudlFire(){
         return StageManager.currentStage.player.keys.magenta;
     }
+
+    _onTileRemoved(x, y){
+        ParticleSpawner.createExplosion(MagentaSparkParticle, x, y, Util.randInt(20, 40)); 
+    }
 }
 
 class CyanDoorBeacon extends DoorBeacon {
     _shoudlFire(){
         return StageManager.currentStage.player.keys.cyan;
+    }
+
+    _onTileRemoved(x, y){
+        ParticleSpawner.createExplosion(CyanSparkParticle, x, y, Util.randInt(20, 40)); 
     }
 }
